61c04d58afe39e57d9865080f7794a2d52c77282
MuhammadHabibH1 <160297008+MuhammadHabibH1@users.noreply.github.com>
2024-02-17 10:00:04 +0700

    Add files via upload

---
+++ Pembaca File/Pembaca File/Form1.Designer.vb
@@ -0,0 +1,102 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button_pencarian = New System.Windows.Forms.Button()
+        Me.TreeView_tampilanfolder = New System.Windows.Forms.TreeView()
+        Me.TextBox_pencarian = New System.Windows.Forms.TextBox()
+        Me.TextBox_tampilanfolder = New System.Windows.Forms.TextBox()
+        Me.Label_status = New System.Windows.Forms.Label()
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        Me.SuspendLayout()
+        '
+        'Button_pencarian
+        '
+        Me.Button_pencarian.Location = New System.Drawing.Point(233, 12)
+        Me.Button_pencarian.Name = "Button_pencarian"
+        Me.Button_pencarian.Size = New System.Drawing.Size(118, 29)
+        Me.Button_pencarian.TabIndex = 0
+        Me.Button_pencarian.Text = "Button1"
+        Me.Button_pencarian.UseVisualStyleBackColor = True
+        '
+        'TreeView_tampilanfolder
+        '
+        Me.TreeView_tampilanfolder.Location = New System.Drawing.Point(13, 47)
+        Me.TreeView_tampilanfolder.Name = "TreeView_tampilanfolder"
+        Me.TreeView_tampilanfolder.Size = New System.Drawing.Size(416, 403)
+        Me.TreeView_tampilanfolder.TabIndex = 1
+        '
+        'TextBox_pencarian
+        '
+        Me.TextBox_pencarian.Location = New System.Drawing.Point(13, 13)
+        Me.TextBox_pencarian.Multiline = True
+        Me.TextBox_pencarian.Name = "TextBox_pencarian"
+        Me.TextBox_pencarian.Size = New System.Drawing.Size(214, 28)
+        Me.TextBox_pencarian.TabIndex = 2
+        '
+        'TextBox_tampilanfolder
+        '
+        Me.TextBox_tampilanfolder.Location = New System.Drawing.Point(439, 94)
+        Me.TextBox_tampilanfolder.Multiline = True
+        Me.TextBox_tampilanfolder.Name = "TextBox_tampilanfolder"
+        Me.TextBox_tampilanfolder.Size = New System.Drawing.Size(353, 96)
+        Me.TextBox_tampilanfolder.TabIndex = 3
+        '
+        'Label_status
+        '
+        Me.Label_status.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.Label_status.Location = New System.Drawing.Point(439, 50)
+        Me.Label_status.Name = "Label_status"
+        Me.Label_status.Size = New System.Drawing.Size(357, 31)
+        Me.Label_status.TabIndex = 4
+        Me.Label_status.Text = "Label1"
+        '
+        'OpenFileDialog1
+        '
+        Me.OpenFileDialog1.FileName = "OpenFileDialog1"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(808, 459)
+        Me.Controls.Add(Me.Label_status)
+        Me.Controls.Add(Me.TextBox_tampilanfolder)
+        Me.Controls.Add(Me.TextBox_pencarian)
+        Me.Controls.Add(Me.TreeView_tampilanfolder)
+        Me.Controls.Add(Me.Button_pencarian)
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button_pencarian As Button
+    Friend WithEvents TreeView_tampilanfolder As TreeView
+    Friend WithEvents TextBox_pencarian As TextBox
+    Friend WithEvents TextBox_tampilanfolder As TextBox
+    Friend WithEvents Label_status As Label
+    Friend WithEvents OpenFileDialog1 As OpenFileDialog
+End Class
---
+++ Pembaca File/Pembaca File/Form1.vb
@@ -0,0 +1,66 @@
+﻿Imports System.IO
+'Nama : Muhammad Habib Hidayatulloh
+'Nim  : 230401010097
+'Prodi: S1 - PJJ Informatika
+'Kelas: IT102
+Public Class Form1
+    ' Event handler untuk membuka dialog pemilihan folder
+    Private Sub Button_Browse_Click(sender As Object, e As EventArgs) Handles Button_pencarian.Click
+        ' Tampilkan dialog pemilihan folder
+        Dim folderDialog As New FolderBrowserDialog()
+        Dim result As DialogResult = folderDialog.ShowDialog()
+
+        ' Periksa apakah pengguna telah memilih folder
+        If result = DialogResult.OK Then
+            ' Panggil fungsi untuk menampilkan struktur folder pada TreeView
+            DisplayFolderStructure(folderDialog.SelectedPath)
+        End If
+    End Sub
+
+    ' Fungsi untuk menampilkan struktur folder pada TreeView
+    Private Sub DisplayFolderStructure(folderPath As String)
+        ' Bersihkan TreeView_tampilanfolder sebelum menampilkan struktur folder baru
+        TreeView_tampilanfolder.Nodes.Clear()
+
+        ' Tambahkan node root untuk folder yang dipilih
+        Dim rootNode As TreeNode = TreeView_tampilanfolder.Nodes.Add(folderPath)
+        ' Panggil fungsi untuk menambahkan subfolder dan file ke TreeView
+        AddSubfoldersAndFiles(rootNode, folderPath)
+
+        ' Periksa apakah folder dapat diakses
+        If Directory.Exists(folderPath) Then
+            Label_status.Text = "Folder Ditemukan"
+        Else
+            Label_status.Text = "Folder Tidak Ditemukan"
+        End If
+    End Sub
+
+    ' Fungsi rekursif untuk menambahkan subfolder dan file ke TreeView
+    Private Sub AddSubfoldersAndFiles(parentNode As TreeNode, folderPath As String)
+        ' Periksa apakah folder yang dipilih ada
+        If Directory.Exists(folderPath) Then
+            ' Ambil daftar subfolder dari folder yang dipilih
+            Dim subfolders As String() = Directory.GetDirectories(folderPath)
+            ' Tambahkan setiap subfolder sebagai node ke TreeView
+            For Each subfolder As String In subfolders
+                Dim subfolderNode As TreeNode = parentNode.Nodes.Add(Path.GetFileName(subfolder))
+                ' Panggil fungsi rekursif untuk menambahkan sub-subfolder dan file
+                AddSubfoldersAndFiles(subfolderNode, subfolder)
+            Next
+            ' Ambil daftar file dari folder yang dipilih
+            Dim files As String() = Directory.GetFiles(folderPath)
+            ' Tambahkan setiap file sebagai node ke TreeView
+            For Each file As String In files
+                Dim fileNode As TreeNode = parentNode.Nodes.Add(Path.GetFileName(file))
+            Next
+        End If
+    End Sub
+
+    ' Event handler untuk menampilkan path file yang dipilih di TextBox
+    Private Sub TreeView_tampilanfolder_AfterSelect(sender As Object, e As TreeViewEventArgs) Handles TreeView_tampilanfolder.AfterSelect
+        ' Jika yang dipilih adalah node file, tampilkan path-nya di TextBox
+        If Not e.Node.Nodes.Count > 0 Then
+            TextBox_tampilanfolder.Text = e.Node.FullPath
+        End If
+    End Sub
+End Class
